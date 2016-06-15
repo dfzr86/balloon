a96999d9e1b23888f62c49d20294350db52d752c
Hanxu <1547213@qq.com>
2016-06-15 23:05:49 +0800

    init And Add ignore

---
+++ Balloon/Balloon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Balloon
+//
+//  Created by __zimu on 16/6/15.
+//  Copyright © 2016年 __zimu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
